ce36bb3c677985c6efd8753b4e9261b90d04f2eb
abidindarici <114923176+abidindarici@users.noreply.github.com>
2022-11-07 15:16:50 +0300

    ASP.NET Lessons
    How I learn ASP.NET - Lessons

---
+++ Lesson1MyAspNetCoreProject/Lesson1MyAspNetCoreProject.Web/Controllers/OrnekController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lesson1MyAspNetCoreProject.Web.Controllers
+{
+    public class OrnekController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            ViewBag.name = "ahmet";
+            TempData["surname"] = "yıldız";
+            TempData["name"] = "osman";
+
+
+
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lesson1MyAspNetCoreProject/Lesson1MyAspNetCoreProject.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson1MyAspNetCoreProject.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
